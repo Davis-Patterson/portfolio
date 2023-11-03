75064d32535852e510d990e87ae172a5532a7e29
Davis-Patterson <davisfpatterson@gmail.com>
2023-11-03 02:21:13 -0400

    initial commit of portfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className='title-name'>Davis Patterson</h1>
+      <h1 className='title-subtext'>Software Developer</h1>
+    </>
+  );
+}
+
+export default App;
