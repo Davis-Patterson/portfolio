52e0c0886b3d20659da87a7f8ae0f8beddf72786
Davis-Patterson <davisfpatterson@gmail.com>
2023-11-03 02:39:51 -0400

    subtext edit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className='title-name'>Davis Patterson</h1>
-      <h1 className='title-subtext'>Software Developer</h1>
+      <h1 className='title-subtext'>{"<Software Developer />"}</h1>
     </>
   );
 }
